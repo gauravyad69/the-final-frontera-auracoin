ce55478af1e4cf13c5af96b14243f8a5acc02821
Gaurav <gauravyad2077@gmail.com>
2024-11-04 23:03:30 +0545

    fixed ui and expand

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
+import WebApp from '@twa-dev/sdk';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, BottomNavComponent],
-  template: `
-    <div class="app-container">
-      <router-outlet></router-outlet>
-      <app-bottom-nav></app-bottom-nav>
-    </div>
-  `,
-  styles: [`
-    .app-container {
-      min-height: 100vh;
-      background-color: #121212;
-      color: white;
-      padding-bottom: 60px;
-    }
-  `]
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  ngOnInit(){
+    WebApp.expand();
+  }
+  
+}
